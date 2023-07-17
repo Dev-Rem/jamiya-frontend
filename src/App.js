4e5fe5ad37a60718526eb544b2a6db83adf7d281
Aremu Oluwaseyi Festus <oluwaremu1407@gmail.com>
2023-07-17 09:26:55 +0100

    Restrict route paths based on signed in user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import "./assets/css/style.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import DailyReport from "./pages/DailyReport";
 import NewTransaction from "./pages/NewTransaction";
@@ -44,90 +39,98 @@ export default function App() {
           path={"/:section/view-transaction"}
           element={<TransactionDetails />}
         />
-
         <Route
           exact
           path={"/:receiptnumber/transaction-receipt"}
           element={<PreviewReceipt />}
         />
+        {JSON.parse(localStorage.getItem("user")).is_staff === false ? (
+          <>
+            <Route
+              exact
+              path={"/:section/update-balances"}
+              element={<UpdateBalances />}
+            />
+            <Route exact path={"/report"} element={<DailyReport />} />
 
-        {/* {JSON.parse(localStorage.getItem("user")).is_staff === false ? ( */}
-        <>
-          <Route
-            exact
-            path={"/:section/update-balances"}
-            element={<UpdateBalances />}
-          />
-          <Route exact path={"/report"} element={<DailyReport />} />
-
-          <Route exact path={"/new-transaction"} element={<NewTransaction />} />
-          <Route exact path={"/my-transactions"} element={<MyTransactions />} />
-        </>
-        {/* ) : (
-          <></>
-        )} */}
-
+            <Route
+              exact
+              path={"/new-transaction"}
+              element={<NewTransaction />}
+            />
+            <Route
+              exact
+              path={"/my-transactions"}
+              element={<MyTransactions />}
+            />
+          </>
+        ) : (
+          <Route exact path={"/dashboard"} element={<Dashboard />} />
+        )}
         {/* admin  routes */}
-        {/* {JSON.parse(localStorage.getItem("user")).is_admin === true ? ( */}
-        <>
-          <Route exact path={"/report-logs"} element={<ReportList />} />
-          <Route
-            exact
-            path={"/transaction-logs"}
-            element={<TransactionLog />}
-          />
-          <Route exact path={"/report-details"} element={<ReportDetails />} />
-          <Route
-            exact
-            path={"/:search/report/search-results"}
-            element={<SearchedReportResults />}
-          />
-          <Route
-            exact
-            path={"/:search/transaction/search-results"}
-            element={<SearchedTransactionResults />}
-          />
-        </>
-        {/* ) : (
-          <></>
-        )} */}
-
+        {JSON.parse(localStorage.getItem("user")).is_admin === true ? (
+          <>
+            <Route exact path={"/report-logs"} element={<ReportList />} />
+            <Route
+              exact
+              path={"/transaction-logs"}
+              element={<TransactionLog />}
+            />
+            <Route exact path={"/report-details"} element={<ReportDetails />} />
+            <Route
+              exact
+              path={"/:search/report/search-results"}
+              element={<SearchedReportResults />}
+            />
+            <Route
+              exact
+              path={"/:search/transaction/search-results"}
+              element={<SearchedTransactionResults />}
+            />
+          </>
+        ) : (
+          <Route exact path={"/dashboard"} element={<Dashboard />} />
+        )}
         {/* admin and staff routes */}
-        {/* {JSON.parse(localStorage.getItem("user")).is_admin === true &&
-        JSON.parse(localStorage.getItem("user")).is_staff === true ? ( */}
-        <>
-          <Route exact path={"/general-ledger"} element={<GeneralLedger />} />
-          <Route
-            exact
-            path={"/ledger-details"}
-            element={<ViewGeneralLedger />}
-          />
-          <Route
-            exact
-            path={"/:section/add-account"}
-            element={<NewAccount />}
-          />
-          <Route exact path={"/:section/update-rates"} element={<Rate />} />
-          <Route exact path={"/:section/add"} element={<AddCustomerLedger />} />
-          <Route
-            exact
-            path={"/:section/view"}
-            element={<ViewCustomerLedger />}
-          />
-          <Route
-            exact
-            path={"/:section/update-account"}
-            element={<EditOrDeleteAccount />}
-          />
-          <Route
-            exact
-            path={"/customer-ledger"}
-            element={<CustomerLedgerPage />}
-          />
-        </>
-        {/* ) : (
-          <></>
-        )} */}
+        {JSON.parse(localStorage.getItem("user")).is_admin === true &&
+        JSON.parse(localStorage.getItem("user")).is_staff === true ? (
+          <>
+            <Route exact path={"/general-ledger"} element={<GeneralLedger />} />
+            <Route
+              exact
+              path={"/ledger-details"}
+              element={<ViewGeneralLedger />}
+            />
+            <Route
+              exact
+              path={"/:section/add-account"}
+              element={<NewAccount />}
+            />
+            <Route exact path={"/:section/update-rates"} element={<Rate />} />
+            <Route
+              exact
+              path={"/:section/add"}
+              element={<AddCustomerLedger />}
+            />
+            <Route
+              exact
+              path={"/:section/view"}
+              element={<ViewCustomerLedger />}
+            />
+            <Route
+              exact
+              path={"/:section/update-account"}
+              element={<EditOrDeleteAccount />}
+            />
+            <Route
+              exact
+              path={"/customer-ledger"}
+              element={<CustomerLedgerPage />}
+            />
+          </>
+        ) : (
+          <Route exact path={"/dashboard"} element={<Dashboard />} />
+        )}
       </Routes>
     </Router>
   );
